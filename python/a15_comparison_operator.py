cecd375a847322b2a9d651c002fddb0081532e68
freshmea <freshmea@gmail.com>
2026-06-23 09:46:18 +0900

    비교 연산자

---
+++ python/a15_comparison_operator.py
@@ -0,0 +1,9 @@
+def main():
+    print(10 == 100)    # False
+    print(10 != 100)    # True
+    print(10 < 100)     # True
+    print(100 <= 100)   # True
+
+
+if __name__ == "__main__":
+    main()
