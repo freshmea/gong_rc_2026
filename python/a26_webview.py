c3a30d510205ef84681518d484ec47fc0a602da9
freshmea <freshmea@gmail.com>
2026-06-23 16:51:05 +0900

    webview

---
+++ python/a26_webview.py
@@ -0,0 +1,19 @@
+# pip install pywebview
+from pathlib import Path
+
+import webview
+
+
+def main():
+    webview.create_window(
+        "simple text",
+        url=(Path(__file__).resolve().parent / "text.html").as_uri(),
+        width=640,
+        height=520,
+        resizable=True
+        )
+    webview.start()
+
+
+if __name__ == "__main__":
+    main()
