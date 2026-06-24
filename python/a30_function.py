f87d7584877217426a6b2dbb8fe2494861debb48
freshmea <freshmea@gmail.com>
2026-06-24 13:13:53 +0900

    강의 계획서

--- python/a30_function.py
+++ python/a30_function.py
@@ -1,4 +1,4 @@
-def print_hello(a: int, value: str | int) -> str:
+def print_hello(a: int, value: str | int):
     """이 함수는 수업에서 작성한 예시 함수
 
     Args:
@@ -13,12 +13,14 @@ def print_hello(a: int, value: str | int) -> str:
     if isinstance(value, int):  # python 에서 type 을 체크 할때!!
         for _ in range(value):
             print("test")
-    return "execution OK!"
+    return "execution OK!", 123, 3.14
 
 
 def main():
-    re = print_hello(3, 2)    # type hint, positional argument
-    print(re)
+    re, re1, _ = print_hello(3, 2)    # type hint, positional argument
+    print(re, re1)
+    re = print_hello(3, 2)
+    print(*re)      # tuple 을 unpack 해서 풀어서 사용
 
 
 if __name__ == "__main__":
