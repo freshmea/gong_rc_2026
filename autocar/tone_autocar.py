0f995bd5ab1e939d752d0fa0027cc03570ff0aa2
freshmea <freshmea@gmail.com>
2026-06-30 15:54:17 +0900

    self

--- autocar/tone_autocar.py
+++ autocar/tone_autocar.py
@@ -1,26 +1,10 @@
-
-import numpy as np
-import pyaudio
-
-volume = 0.5
-fs = 48000
-duration = 5.0
-f = 440.0       # 라 음
-
-
-
-p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paFloat32, channels=1, rate=fs, output=True)
-stream.write(volume * data)
-
-
 class Tone:
     def __init__(self, volume=0.5, rate=48000, channels=1):
         self.volume = volume
         self.rate = rate
         self.channels = channels
         self.p = pyaudio.PyAudio()
-        self.stream = p.open(format=pyaudio.paFloat32, channels=self.channels, rate=self.rate, output=True)
+        self.stream = self.p.open(format=pyaudio.paFloat32, channels=self.channels, rate=self.rate, output=True)
 
     def play(self, octave=3, note=1, duration=2):
         f = 2**(octave) * 55 * 2**(((note)-10)/12)
