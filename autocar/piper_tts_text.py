4301a4e7e18a909a85e1c9490c66569282edc067
freshmea <freshmea@gmail.com>
2026-07-01 10:49:12 +0900

    piper

---
+++ autocar/piper_tts_text.py
@@ -0,0 +1,4 @@
+# wget https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/lessac/medium/en_US-lessac-medium.onnx
+# wget https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/lessac/medium/en_US-lessac-medium.onnx.json
+# echo "Hello, this is Piper text to speech." | \
+# piper --model ~/piper_models/en_US-lessac-medium.onnx --output_file hello.wav
