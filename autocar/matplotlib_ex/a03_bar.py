7be831990017e9b98038f056c7752b87b67bcdf5
freshmea <freshmea@gmail.com>
2026-07-03 14:10:11 +0900

    legend, title, label

--- autocar/matplotlib_ex/a03_bar.py
+++ autocar/matplotlib_ex/a03_bar.py
@@ -21,6 +21,14 @@ def main():
     ax2.bar(np.arange(0, 50, 5), data[0], color="lightgray")
     ax2.bar(np.arange(0, 50, 5) + 1, data[1], color="gray")
     ax2.bar(np.arange(0, 50, 5) + 2, data[2], color="black")
+    fig3 = plt.figure(figsize=(5, 5))
+    ax1 = fig3.add_subplot(2, 1, 1)
+    ax1.bar(np.arange(10), data[0], color="gray")
+    ax1.bar(np.arange(10), data[1], color="lightgray", bottom=data[0])
+    ax1.bar(np.arange(10), data[2], color="black", bottom=(data[1]+data[0]))
+    ax2 = fig3.add_subplot(2, 1, 2)
+    ax2.bar(np.arange(10), data[0], color="gray")
+    ax2.bar(np.arange(10), -data[1], color="black")
     plt.show()
 
 
